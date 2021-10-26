220b4af87426dfcc212448aaea839872027f8d86
Telepath <dayveed.david@gmail.com>
2021-10-26 21:51:45 +0100

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+extern crate num;
+use num::bigint::BigInt;
+use num::Integer;
+use num::One;
+use num::Zero;
+
+// The Euclidean Algorithm is a set of instructions for finding the greatest common divisor
+// of any two positive integers.
+
+// Computes the modular inverse of n modulo p.
+// Creates and initializes a BigInt.
+// Modular multiplicative inverse function for big (negative) numbers
+fn modinv(n: &BigInt, p: &BigInt) -> BigInt {
+    if p.is_one() {
+        return BigInt::one();
+    }
+
+    let (mut a, mut m, mut x, mut inv) = (n.clone(), p.clone(), BigInt::zero(), BigInt::one());
+
+    while a < BigInt::zero() {
+        a += p
+    }
+
+    while a > BigInt::one() {
+        let (div, rem) = a.div_rem(&m);
+        inv -= div * &x;
+        a = rem;
+        std::mem::swap(&mut a, &mut m);
+        std::mem::swap(&mut x, &mut inv);
+    }
+
+    if inv < BigInt::zero() {
+        inv += p
+    }
+    inv
+}
+
+fn main() {
+    let n = BigInt::parse_bytes(
+        b"-243772585612020160733370897338805215918303827399330592839196552441720391139",
+        10,
+    )
+    .unwrap();
+    let p = BigInt::parse_bytes(
+        b"115792089237316195423570985008687907853269984665640564039457584007908834671663",
+        10,
+    )
+    .unwrap();
+
+    println!("modinv({0}, {1}) = {2}", n, p, modinv(&n, &p));
+}
